0ce9408780b503da3e1f025d19827cf550a7e253
artem_zaitsev_2 <artem.zaitsev2017@yandex.ru>
2025-05-17 15:26:35 +0300

    Refactored HTML into Thymeleaf templates and fragments, implemented dynamic rendering, optimized index.html size. Set up PostgreSQL locally, added connection config to Spring Boot, and fixed database startup issues.

--- src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -1,5 +1,6 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.Book;
 import com.example.MyBookShopApp.data.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Controller
-@RequestMapping("/bookshop")
+//@RequestMapping("/bookshop")
 public class MainPageController {
     private final BookService bookService;
 
@@ -20,13 +22,19 @@ public class MainPageController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/main")
-    public String mainPage(Model model){
-        model.addAttribute("bookData", bookService.getBooksData());
-        model.addAttribute("searchPlaceholder", "new search placeholder");
-        model.addAttribute("serverTime", new SimpleDateFormat("hh:mm:ss").format(new Date()));
-        model.addAttribute("placeholderTextPart2", "SERVER");
-        model.addAttribute("messageTemplate","searchbar.placeholder2");
+    @GetMapping("recommendedBooks")
+    public List<Book> recommendedBooks(){
+        return bookService.getBooksData();
+    }
+
+
+    @GetMapping("/") // /main
+    public String mainPage(){ //(Model model)
+//        model.addAttribute("bookData", bookService.getBooksData());
+//        model.addAttribute("searchPlaceholder", "new search placeholder");
+//        model.addAttribute("serverTime", new SimpleDateFormat("hh:mm:ss").format(new Date()));
+//        model.addAttribute("placeholderTextPart2", "SERVER");
+//        model.addAttribute("messageTemplate","searchbar.placeholder2");
         return "index";
     }
 }
